24df8b515e584c1683e1f32ec9c96e90e9d430ab
Stuart Morris <admin@handcode.co.uk>
2019-06-01 10:17:25 +0100

    Basic redux setup

---
+++ client/src/reducers/auth.js
@@ -0,0 +1,14 @@
+const initialState = {
+    token: localStorage.getItem("token"),
+    isAuthenticated: null,
+    loading: true,
+    user: null,
+};
+
+export default function(state = initialState, action) {
+    const { type, payload } = action;
+    switch (type) {
+        default:
+            return state;
+    }
+}
